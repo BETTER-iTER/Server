c88e41b49a24920a2f60765f05fc327733e69cd3
choidongkuen <danaver12@daum.net>
2023-10-18 15:11:17 +0900

    REFACTOR : BaseEntity 변경 및 User 엔티티 상속

--- src/main/java/com/example/betteriter/global/common/entity/BaseEntity.java
+++ src/main/java/com/example/betteriter/global/common/entity/BaseEntity.java
@@ -4,10 +4,14 @@ package com.example.betteriter.global.common.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+
+import static com.example.betteriter.global.common.entity.Status.ACTIVE;
 
 @Getter
 @MappedSuperclass
@@ -15,12 +19,15 @@ import javax.persistence.*;
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
 
-    @Embedded
-    private DateTime dateTime;
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    @Column(name = "status", nullable = false)
-    @ColumnDefault("'ACTIVE'")
-    private Status status;
+    private Status status = ACTIVE;
 }
