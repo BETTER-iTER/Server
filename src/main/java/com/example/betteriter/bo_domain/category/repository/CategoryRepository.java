eee0cbcf2ec90208cca789e20a1cac9d171b4c26
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2023-10-19 21:48:46 +0900

    FEAT: Category 도메인 생성(#32)

---
+++ src/main/java/com/example/betteriter/bo_domain/category/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.example.betteriter.bo_domain.category.repository;
+
+import com.example.betteriter.bo_domain.category.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
