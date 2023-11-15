f4e256799e78261724d550257950d8e1279682ff
choidongkuen <danaver12@daum.net>
2023-11-15 19:37:52 +0900

    FEAT : RedisConfig 검색 전용 RedisTemplate 추가 구현

--- src/main/java/com/example/betteriter/global/config/web/RedisConfig.java
+++ src/main/java/com/example/betteriter/global/config/web/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.example.betteriter.global.config.web;
 
+import com.example.betteriter.fo_domain.search.dto.SearchLogRedis;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
@@ -22,11 +23,23 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<Object>(Object.class));
+        return redisTemplate;
+    }
+
+    /* 최근 검색 작업을 위한 RedisTemplate */
+    @Bean
+    public RedisTemplate<String, SearchLogRedis> searchRecentLogRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, SearchLogRedis> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(SearchLogRedis.class));
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(SearchLogRedis.class));
         return redisTemplate;
     }
 }
