4650be1aed0fe67392b6873177000fe8dc5c0439
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2023-11-25 20:11:20 +0900

    DOCS: comment api swagger 명세 작성 (#92)

--- src/main/java/com/example/betteriter/fo_domain/comment/dto/CommentRequest.java
+++ src/main/java/com/example/betteriter/fo_domain/comment/dto/CommentRequest.java
@@ -3,6 +3,7 @@ package com.example.betteriter.fo_domain.comment.dto;
 import com.example.betteriter.fo_domain.comment.validation.annotation.ExistComment;
 import com.example.betteriter.fo_domain.comment.validation.annotation.UserHaveComment;
 import com.example.betteriter.fo_domain.review.validation.annotation.ExistReview;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,7 +18,10 @@ public class CommentRequest {
     public static class CreateCommentDto {
 
         @ExistReview
+        @Schema(description = "리뷰 아이디", example = "1")
         private Long review_id;
+
+        @Schema(description = "댓글 내용", example = "댓글 내용")
         private String comment;
     }
 
@@ -28,6 +32,8 @@ public class CommentRequest {
     public static class DeleteCommentDto {
 
         @ExistComment
+        @UserHaveComment
+        @Schema(description = "댓글 아이디", example = "1")
         private Long comment_id;
 
     }
