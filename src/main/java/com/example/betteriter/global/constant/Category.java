b641d99ca3bef0dfff376ac73437d67714145bad
choidongkuen <danaver12@daum.net>
2023-11-30 00:57:59 +0900

    REFACTOR : Category enum class 리팩토링 (#101)

--- src/main/java/com/example/betteriter/global/constant/Category.java
+++ src/main/java/com/example/betteriter/global/constant/Category.java
@@ -1,12 +1,14 @@
 package com.example.betteriter.global.constant;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 // 사용자 회원 가입 시 name 을 입력 받아 enum 생성 후 저장
 // 조회 시에도 이름 + url
+
 @Getter
-@RequiredArgsConstructor
 public enum Category {
     MOBILE_PHONE("휴대폰", "휴대폰 이미지 URL"),
     LAPTOP("노트북", "노트북 이미지 URL"),
@@ -23,4 +25,25 @@ public enum Category {
 
     private final String name;
     private final String imageUrl;
+
+    Category(String name, String imageUrl) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+    }
+
+    /* 요청으로 들어온 문자열이 Enum 타입으로 역직렬화될때 메소드의 매개변수로 들어감 */
+    @JsonCreator
+    public static Category from(@JsonProperty("category") String name) {
+        for (Category category : Category.values()) {
+            if (category.name.equals(name)) {
+                return category;
+            }
+        }
+        throw new IllegalArgumentException("일치하는 Category 정보가 존재하지 않습니다.");
+    }
+
+    @JsonValue
+    public String getCategoryName() {
+        return name;
+    }
 }
