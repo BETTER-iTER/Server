921054f59da3923bb9676983f097c3da0edc2d44
choidongkuen <danaver12@daum.net>
2023-12-04 20:24:02 +0900

    TEST : 카테고리 별 리뷰 조회 API 서비스,레포지토리 단위 테스트 (#108)

--- src/test/java/com/example/betteriter/fo_domain/review/repository/ReviewRepositoryTest.java
+++ src/test/java/com/example/betteriter/fo_domain/review/repository/ReviewRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.example.betteriter.fo_domain.review.repository;
 
 import com.example.betteriter.fo_domain.review.domain.Review;
+import com.example.betteriter.fo_domain.review.domain.ReviewLike;
 import com.example.betteriter.fo_domain.review.domain.ReviewScrap;
 import com.example.betteriter.fo_domain.user.domain.Users;
 import com.example.betteriter.fo_domain.user.repository.UsersRepository;
@@ -13,8 +14,10 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 
+import javax.persistence.EntityManager;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import static com.example.betteriter.global.constant.Category.PC;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,6 +27,9 @@ import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTest
 @AutoConfigureTestDatabase(replace = NONE)
 @ActiveProfiles("test")
 public class ReviewRepositoryTest {
+
+    @Autowired
+    private EntityManager em;
 
     @Autowired
     private ReviewRepository reviewRepository;
@@ -204,6 +210,236 @@ public class ReviewRepositoryTest {
         // then
         for (Review review : result) {
             System.out.println("review = " + review);
+        }
+    }
+
+    @Test
+    @DisplayName("카테고리 별 리뷰 조회을 한다.")
+    void findReviewsByCategoryByDesc() {
+        // given
+
+        // 1. 유저 저장
+        Users user01 = Users.builder()
+                .oauthId("oauthId")
+                .email("danaver12@daum.net")
+                .password("1234")
+                .roleType(RoleType.ROLE_USER)
+                .isExpert(true)
+                .build();
+
+        Users user02 = Users.builder()
+                .oauthId("oauthId02")
+                .email("danaver12@naver.net")
+                .password("5678")
+                .roleType(RoleType.ROLE_USER)
+                .isExpert(true)
+                .build();
+
+        Users user03 = Users.builder()
+                .oauthId("oauthId03")
+                .email("danaver12@kakao.net")
+                .password("11111")
+                .roleType(RoleType.ROLE_USER)
+                .isExpert(true)
+                .build();
+
+        this.usersRepository.saveAll(List.of(user01, user02, user03));
+
+        // 2. 리뷰 저장
+        Review review01 = Review.builder()
+                .category(PC)
+                .productName("productName01")
+                .amount(100000)
+                .storeName(1)
+                .boughtAt(LocalDate.now())
+                .starPoint(0)
+                .shortReview("shortReview")
+                .goodPoint("goodPoint")
+                .badPoint("badPoint")
+                .build();
+
+        Review review02 = Review.builder()
+                .category(PC)
+                .productName("productName02")
+                .amount(100000)
+                .storeName(1)
+                .boughtAt(LocalDate.now())
+                .starPoint(0)
+                .shortReview("shortReview")
+                .goodPoint("goodPoint")
+                .badPoint("badPoint")
+                .build();
+
+        Review review03 = Review.builder()
+                .category(PC)
+                .productName("productName03")
+                .amount(100000)
+                .storeName(1)
+                .boughtAt(LocalDate.now())
+                .starPoint(0)
+                .shortReview("shortReview")
+                .goodPoint("goodPoint")
+                .badPoint("badPoint")
+                .build();
+
+        Review review04 = Review.builder()
+                .category(PC)
+                .productName("productName04")
+                .amount(100000)
+                .storeName(1)
+                .boughtAt(LocalDate.now())
+                .starPoint(0)
+                .shortReview("shortReview")
+                .goodPoint("goodPoint")
+                .badPoint("badPoint")
+                .build();
+
+        this.reviewRepository.saveAll(List.of(review01, review02, review03, review04));
+
+        // 3. 리뷰 스크랩 + 좋아요 저장
+        // review01 스크랩
+        ReviewScrap reviewScrap01 = ReviewScrap.builder()
+                .review(review01)
+                .users(user01)
+                .build();
+
+        // review02 스크랩
+        ReviewScrap reviewScrap02 = ReviewScrap.builder()
+                .review(review02)
+                .users(user01)
+                .build();
+
+        // review03 스크랩
+        ReviewScrap reviewScrap03 = ReviewScrap.builder()
+                .review(review03)
+                .users(user01)
+                .build();
+
+        // review01 스크랩
+        ReviewScrap reviewScrap04 = ReviewScrap.builder()
+                .review(review01)
+                .users(user02)
+                .build();
+
+        // review02 스크랩
+        ReviewScrap reviewScrap05 = ReviewScrap.builder()
+                .review(review02)
+                .users(user02)
+                .build();
+
+        // review01 스크랩
+        ReviewScrap reviewScrap06 = ReviewScrap.builder()
+                .review(review01)
+                .users(user03)
+                .build();
+
+        // review01 좋아요
+        ReviewLike reviewLike01 = ReviewLike.builder()
+                .review(review01)
+                .users(user01)
+                .build();
+
+        // review02 좋아요
+        ReviewLike reviewLike02 = ReviewLike.builder()
+                .review(review02)
+                .users(user02)
+                .build();
+
+        // review03 좋아요
+        ReviewLike reviewLike03 = ReviewLike.builder()
+                .review(review01)
+                .users(user03)
+                .build();
+
+        // review02 좋아요
+        ReviewLike reviewLike04 = ReviewLike.builder()
+                .review(review02)
+                .users(user03)
+                .build();
+
+        // review04 좋아요
+        ReviewLike reviewLike05 = ReviewLike.builder()
+                .review(review04)
+                .users(user02)
+                .build();
+
+        System.out.println("reviewLiked 저장");
+        this.reviewLikeRepository.saveAll(List.of(reviewLike01, reviewLike02, reviewLike03, reviewLike04, reviewLike05));
+        System.out.println("reviewScrap 저장");
+        this.reviewScrapRepository.saveAll(List.of(reviewScrap01, reviewScrap02, reviewScrap03, reviewScrap04, reviewScrap05, reviewScrap05, reviewScrap06));
+
+
+        // when
+        Optional<Review> result01 = this.reviewRepository.findById(1L); // 영속성 컨텍스트에서 가져옴
+
+
+        em.flush();
+        em.clear();
+
+        System.out.println("jpql 실행");
+        List<Review> result
+                = this.reviewRepository.findReviewByCategory(PC, PageRequest.of(0, 5)).getContent();
+        System.out.println("jpql 실행 끝");
+
+        // then
+        for (Review review : result) {
+            System.out.println("review.getId() = " + review.getId());
+            System.out.println(review.getReviewScraped().get(0));
+        }
+    }
+
+    @Test
+    @DisplayName("리뷰 관련 JPA 테스트을 진행한다.")
+    void testjpa() {
+        // given
+        // 1. 유저 저장
+        Users user01 = Users.builder()
+                .oauthId("oauthId")
+                .email("danaver12@daum.net")
+                .password("1234")
+                .roleType(RoleType.ROLE_USER)
+                .isExpert(true)
+                .build();
+
+        Users user = this.usersRepository.save(user01);
+
+        // 1. 리뷰 저장
+        Review review01 = Review.builder()
+                .category(PC)
+                .productName("productName01")
+                .amount(100000)
+                .storeName(1)
+                .boughtAt(LocalDate.now())
+                .starPoint(0)
+                .shortReview("shortReview")
+                .goodPoint("goodPoint")
+                .badPoint("badPoint")
+                .build();
+
+
+        ReviewLike reviewLike01 = ReviewLike.builder()
+                .users(user)
+                .review(review01)
+                .build();
+
+        ReviewLike reviewLike02 = ReviewLike.builder()
+                .users(user)
+                .review(review01)
+                .build();
+
+        review01.setReviewLiked(List.of(reviewLike01, reviewLike02));
+        Review saveReview = this.reviewRepository.save(review01);
+
+        em.flush();
+        em.clear();
+
+        Review foundReview = this.reviewRepository.findById(1L).get();
+        System.out.println(foundReview.getReviewLiked().size());
+        List<ReviewLike> reviewLiked = foundReview.getReviewLiked();
+
+        for (ReviewLike reviewLike : reviewLiked) {
+            System.out.println(reviewLike.getReview());
+            System.out.println(reviewLike.getUsers().getId());
         }
     }
 }
--- src/test/java/com/example/betteriter/fo_domain/review/service/ReviewServiceTest.java
+++ src/test/java/com/example/betteriter/fo_domain/review/service/ReviewServiceTest.java
@@ -8,24 +8,32 @@ import com.example.betteriter.bo_domain.spec.service.SpecService;
 import com.example.betteriter.fo_domain.review.domain.Review;
 import com.example.betteriter.fo_domain.review.dto.CreateReviewRequestDto;
 import com.example.betteriter.fo_domain.review.dto.CreateReviewRequestDto.CreateReviewImageRequestDto;
+import com.example.betteriter.fo_domain.review.dto.GetCategoryReviewResponseDto;
+import com.example.betteriter.fo_domain.review.dto.GetReviewResponseDto;
 import com.example.betteriter.fo_domain.review.dto.GetReviewSpecResponseDto;
 import com.example.betteriter.fo_domain.review.repository.ReviewImageRepository;
 import com.example.betteriter.fo_domain.review.repository.ReviewRepository;
 import com.example.betteriter.fo_domain.review.repository.ReviewSpecDataRepository;
 import com.example.betteriter.fo_domain.user.domain.Users;
+import com.example.betteriter.fo_domain.user.domain.UsersDetail;
 import com.example.betteriter.fo_domain.user.service.UserService;
 import com.example.betteriter.global.constant.Category;
+import com.example.betteriter.global.constant.Job;
+import com.example.betteriter.global.constant.RoleType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.SliceImpl;
 
 import java.time.LocalDate;
 import java.util.List;
 
 import static com.example.betteriter.global.constant.Category.LAPTOP;
+import static com.example.betteriter.global.constant.Category.PC;
 import static com.example.betteriter.global.constant.RoleType.ROLE_USER;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.*;
@@ -142,5 +150,90 @@ public class ReviewServiceTest {
         // then
         assertThat(reviewSpecResponseDto.getSpecs()).isNotNull();
         assertThat(reviewSpecResponseDto.getSpecs()).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("카테고리에 해당하는 모든 리뷰를 조회한다.")
+    void getReviewByCategory() {
+        // given
+
+        Users user01 = Users.builder()
+                .oauthId("oauthId")
+                .email("danaver12@daum.net")
+                .password("1234")
+                .roleType(RoleType.ROLE_USER)
+                .isExpert(true)
+                .usersDetail(UsersDetail.builder()
+                        .nickName("nickname")
+                        .job(Job.DEVELOPER).build())
+                .build();
+
+
+        Review review01 = Review.builder()
+                .id(1L)
+                .category(PC)
+                .productName("productName01")
+                .amount(100000)
+                .writer(user01)
+                .storeName(1)
+                .boughtAt(LocalDate.now())
+                .starPoint(0)
+                .shortReview("shortReview")
+                .goodPoint("goodPoint")
+                .badPoint("badPoint")
+                .build();
+
+        Review review02 = Review.builder()
+                .id(2L)
+                .category(PC)
+                .productName("productName02")
+                .writer(user01)
+                .amount(100000)
+                .storeName(1)
+                .boughtAt(LocalDate.now())
+                .starPoint(0)
+                .shortReview("shortReview")
+                .goodPoint("goodPoint")
+                .badPoint("badPoint")
+                .build();
+
+        Review review03 = Review.builder()
+                .id(3L)
+                .category(PC)
+                .productName("productName01")
+                .writer(user01)
+                .amount(100000)
+                .storeName(1)
+                .boughtAt(LocalDate.now())
+                .starPoint(0)
+                .shortReview("shortReview")
+                .goodPoint("goodPoint")
+                .badPoint("badPoint")
+                .build();
+
+        Review review04 = Review.builder()
+                .id(4L)
+                .category(PC)
+                .productName("productName01")
+                .writer(user01)
+                .amount(100000)
+                .storeName(1)
+                .boughtAt(LocalDate.now())
+                .starPoint(0)
+                .shortReview("shortReview")
+                .goodPoint("goodPoint")
+                .badPoint("badPoint")
+                .build();
+
+
+        given(reviewRepository.findReviewByCategory(any(Category.class), any(Pageable.class)))
+                .willReturn(new SliceImpl<>(List.of(review01, review02, review03, review04)));
+
+        // when
+        GetCategoryReviewResponseDto result = this.reviewService.getReviewByCategory(PC);
+        // then
+        for (GetReviewResponseDto getReviewResponseDto : result.getGetReviewResponseDtoList()) {
+            System.out.println("getReviewResponseDto = " + getReviewResponseDto);
+        }
     }
 }
