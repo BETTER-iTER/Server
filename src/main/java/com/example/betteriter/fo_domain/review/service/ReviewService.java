6038eef087cad39124bf56d38f5f0b4f8748e0bc
choidongkuen <danaver12@daum.net>
2024-01-08 04:36:39 +0900

    FEAT : 리뷰 스크랩 취소 API 구현 (#145)

--- src/main/java/com/example/betteriter/fo_domain/review/service/ReviewService.java
+++ src/main/java/com/example/betteriter/fo_domain/review/service/ReviewService.java
@@ -175,29 +175,24 @@ public class ReviewService {
         Users currentUser = this.getCurrentUser();
         this.reviewLikeRepository.save(ReviewLike.builder().review(review).users(currentUser).build());
         // 3. 리뷰 좋아요 카운트 증가
-        review.countReviewLikedCount();
+        review.addReviewLikedCount();
     }
 
     /* 리뷰 좋아요 취소 */
     @Transactional
     public Void deleteReviewLike(Long reviewId) {
-        // 1. reviewId 에 해당하는 리뷰 조회
         Review review = this.findReviewById(reviewId);
         ReviewLike reviewLike = checkReviewLikeValidation(review);
         this.reviewLikeRepository.delete(reviewLike);
+        review.minusReviewLikedCount();
         return null;
     }
 
     @NotNull
     private ReviewLike checkReviewLikeValidation(Review review) {
-        // 2. reviewId 해당하는 ReviewLike 엔티티 조회
-        ReviewLike reviewLike = this.reviewLikeRepository.findByReview(review)
+        Users currentUser = this.getCurrentUser();
+        return this.reviewLikeRepository.findByReviewAndUsers(review, currentUser)
                 .orElseThrow(() -> new ReviewHandler(_REVIEW_LIKE_NOT_FOUND));
-        // 3. reviewLike 의 user 와 current 유저 동일한지 체크
-        if (!Objects.equals(reviewLike.getUsers().getId(), this.getCurrentUser().getId())) {
-            throw new ReviewHandler(_REVIEW_LIKE_USER_NOT_MATCH);
-        }
-        return reviewLike;
     }
 
     /* 리뷰 스크랩 */
@@ -209,7 +204,23 @@ public class ReviewService {
         Users currentUser = this.getCurrentUser();
         this.reviewScrapRepository.save(ReviewScrap.builder().review(review).users(currentUser).build());
         // 3. 리뷰 스크랩 카운트 증가
-        review.countReviewScrapedCount();
+        review.addReviewScrapedCount();
+    }
+
+    /* 리뷰 스크랩 취소 */
+    @Transactional
+    public void deleteReviewScrap(Long reviewId) {
+        Review review = this.findReviewById(reviewId);
+        ReviewScrap reviewScrap = checkReviewScrapValidation(review);
+        this.reviewScrapRepository.delete(reviewScrap);
+        review.minusReviewScrapedCount();
+    }
+
+    @NotNull
+    private ReviewScrap checkReviewScrapValidation(Review review) {
+        Users currentUser = this.getCurrentUser();
+        return this.reviewScrapRepository.findByReviewAndUsers(review, currentUser)
+                .orElseThrow(() -> new ReviewHandler(_REVIEW_SCRAP_NOT_FOUND));
     }
 
     /* 리뷰 삭제 */
