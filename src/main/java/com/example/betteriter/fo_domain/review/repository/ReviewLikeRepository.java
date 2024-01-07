009eaeace7e7e500c7a9bb48dc41230c9c23606f
choidongkuen <danaver12@daum.net>
2024-01-08 04:37:06 +0900

    REFACTOR : 리뷰 좋아요,스크랩 API 일부 리팩토링 (#145)

--- src/main/java/com/example/betteriter/fo_domain/review/repository/ReviewLikeRepository.java
+++ src/main/java/com/example/betteriter/fo_domain/review/repository/ReviewLikeRepository.java
@@ -2,10 +2,11 @@ package com.example.betteriter.fo_domain.review.repository;
 
 import com.example.betteriter.fo_domain.review.domain.Review;
 import com.example.betteriter.fo_domain.review.domain.ReviewLike;
+import com.example.betteriter.fo_domain.user.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface ReviewLikeRepository extends JpaRepository<ReviewLike, Long> {
-    Optional<ReviewLike> findByReview(Review review);
+    Optional<ReviewLike> findByReviewAndUsers(Review review, Users users);
 }
