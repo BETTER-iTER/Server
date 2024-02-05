797ac9ee598918db1c781c1ba1e476995f0c5836
choidongkuen <danaver12@daum.net>
2024-02-05 17:46:12 +0900

    feat : 메인 클래스 aws s3 관련 제외 추가 (#163)

--- src/main/java/com/example/betteriter/BetterIterApplication.java
+++ src/main/java/com/example/betteriter/BetterIterApplication.java
@@ -3,7 +3,13 @@ package com.example.betteriter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {
+        io.awspring.cloud.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+        io.awspring.cloud.autoconfigure.context.ContextStackAutoConfiguration.class,
+        io.awspring.cloud.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+    }
+)
 public class BetterIterApplication {
 
     public static void main(String[] args) {
