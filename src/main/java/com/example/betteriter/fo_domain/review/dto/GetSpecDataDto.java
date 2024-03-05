bd57394a513a2345f214c08184d13c99f650f8b8
SunKyu Choi <zjvl324@gmail.com>
2024-03-05 16:00:51 +0900

    FEAT: 직렬화 문제 해결
    <footer>
    - 관련: #191

--- src/main/java/com/example/betteriter/fo_domain/review/dto/GetSpecDataDto.java
+++ src/main/java/com/example/betteriter/fo_domain/review/dto/GetSpecDataDto.java
@@ -1,8 +1,12 @@
 package com.example.betteriter.fo_domain.review.dto;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 @Builder
+@Getter
+@Setter
 public class GetSpecDataDto {
 	private Long SpecId;
 	private Long SpecDataId;
